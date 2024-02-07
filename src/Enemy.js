5aec3cb150a6a0b0ad70267f39265bbfe9d252b0
Deepanshu <deepanshu.9dd@gmail.com>
2024-02-07 11:28:54 +0530

    create enemy shoot algorithm

--- src/Enemy.js
+++ src/Enemy.js
@@ -4,33 +4,55 @@ const c = new Config();
 
 export default class Enemy {
 
-    constructor(angle, earth) {
+    constructor(angle, earth, target, bulletController) {
         this.enemyX = 0;
         this.enemyY = 0;
-        this.angle = angle;
-        this.theta = 0;
+        this.centerX = 0;
+        this.centerY = 0;
+        this.target = target;
+        this.theta = radians(angle);
         this.earth = earth;
-
+        this.bulletController = bulletController;
+        this.color = color(240, 248, 255)
         this.health = 10;
+        this.diameter = 40
     }
 
     update() {
-        this.theta = radians(this.angle);
         this.enemyX = this.earth.earthX + (this.earth.earthDiameter / 2) * Math.cos(this.theta);
-        this.enemyY = this.earth.earthY + (this.earth.earthDiameter / 2) * Math.sin(this.theta);    
+        this.enemyY = this.earth.earthY + (this.earth.earthDiameter / 2) * Math.sin(this.theta);
+        this.shoot();
     }
-    
+
     draw() {
-        fill(240, 248, 255);
+        fill(this.color);
         push();
         translate(this.enemyX, this.enemyY);
         rotate(this.theta);
-        rect(0, 0, 30, 40);
+        circle(0,0,this.diameter);
         pop();
     }
 
-    damage(damage){
-        stroke(2, 5 ,5)
+    damage(damage) {
+        stroke(2, 5, 5)
         this.health -= damage;
     }
+
+    shoot() {
+        this.shootPressed = true;
+        let speed = 15;
+        const damage = 10;
+        const delay = 50;
+        const id = 0;
+
+        let playerAngle = degrees(this.target.theta);
+        if(playerAngle < 0){
+            playerAngle += 360; 
+        }
+
+        if(Math.abs(playerAngle - degrees(this.theta)) <= 10) {
+            this.bulletController.shoot(this.enemyX, this.enemyY, speed, damage, delay, this.theta, id);
+        }
+    }
+
 }
