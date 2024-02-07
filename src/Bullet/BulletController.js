0ceb7756c6ca1cd620983e080ff899c5e298d055
Deepanshu <deepanshu.9dd@gmail.com>
2024-02-07 11:27:45 +0530

    add player collision

--- src/Bullet/BulletController.js
+++ src/Bullet/BulletController.js
@@ -10,45 +10,45 @@ export default class BulletController{
         this.earth = earth;
     }
 
-    shoot(x, y ,speed, damage, delay, theta){
-        if(this.timeTillNextBullet <= 0){
-            this.bullets.push(new Bullet(x, y , damage ,speed, theta));
-            this.timeTillNextBullet = delay;
-        }
-        this.timeTillNextBullet--;
-    }
     draw(){
         this.bullets.forEach((bullet) =>{
             if(this.isBulletOffScreen(bullet)){
                 const index = this.bullets.indexOf(bullet);
                 this.bullets.splice(index, 1) 
             }
-
+            
             this.earth.update()
-            if(this.isBulletOnEarth(bullet,this.earth.earthX, this.earth.earthY, this.earth.earthDiameter)){
+            if(this.isBulletOnEarth(bullet,this.earth.earthX, this.earth.earthY, this.earth.earthDiameter ) && bullet.id == 1){
                 this.bullets.splice(this.bullets.indexOf(bullet), 1)   
             }
-
-         bullet.draw()
+            bullet.draw()
         } )
     }
 
+    shoot(x, y ,speed, damage, delay, theta, id){
+            if(this.timeTillNextBullet <= 0){
+                this.bullets.push(new Bullet(x, y , damage ,speed, theta, id));
+                this.timeTillNextBullet = delay;
+            }
+            this.timeTillNextBullet--;
+    }
+    
     isBulletOffScreen(bullet){
         return bullet.bulletX < 0 || bullet.bulletX > c.canvasWidth || bullet.bulletY < 0 || bullet.bulletY > c.canvasHeight;
     }
 
     isBulletOnEarth(bullet, earthX, earthY ,earthDiameter){
         let distanceToEarth = dist(bullet.bulletX, bullet.bulletY, earthX, earthY);
-        return distanceToEarth <= earthDiameter/2;
+        return distanceToEarth <= earthDiameter/2-2;
     }
 
-    collidedWith(enemy){
+    collidedWith(sprite){
         return this.bullets.some(bullet=>{
-            if(bullet.collideWith(enemy)){
-                this.bullets.splice(this.bullets.indexOf(bullet, 1));
-                return true;
-            }
-            return false;
+                if(bullet.collideWith(sprite)){
+                    this.bullets.splice(this.bullets.indexOf(bullet, 1));
+                    return true;
+                }
+                return false;
         })
     }
 }
