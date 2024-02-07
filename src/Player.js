fda02731872847fcbe6a17c23f049152b1ab6145
Deepanshu <deepanshu.9dd@gmail.com>
2024-02-07 11:29:29 +0530

    change player axis

--- src/Player.js
+++ src/Player.js
@@ -11,6 +11,7 @@ export default class Player{
       this.playerY = 0;
       this.theta = 0;
       this.bulletController = bulletController;
+      this.health = 50;
     }
     
     update(){
@@ -20,7 +21,7 @@ export default class Player{
     
       this.theta =  c.getCenterAngle(c.setCenterAngle(Math.atan2(b, a)));
   
-      this.playerX= c.getCenterX() + (playerMajorAxis  + 140) * Math.cos(this.theta);
+      this.playerX= c.getCenterX() + (playerMinorAxis  + 140) * Math.cos(this.theta);
       this.playerY = c.getCenterY() + (playerMinorAxis + 140) * Math.sin(this.theta);
       this.shoot();
     }
@@ -30,8 +31,13 @@ export default class Player{
       push();
       translate(this.playerX, this.playerY);;
       rotate(this.theta);
-      ellipse(0, 0, 30, 40);
+      ellipse(0, 0, 30);
+      console.log(this.health)
       pop();
+    }
+
+    damage(damage){
+      this.health -= damage;
     }
 
     shoot(){
@@ -40,7 +46,8 @@ export default class Player{
         let speed = 5;
         const damage = 5;
         const delay =10;
-        this.bulletController.shoot(this.playerX, this.playerY, speed, damage, delay, this.theta)
+        const id = 1;
+        this.bulletController.shoot(this.playerX, this.playerY, speed, damage, delay, this.theta, id)
       }
     }
 
