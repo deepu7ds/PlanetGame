d48b0cdd5eda1efbb005887a2788f0d867bcad3c
Deepanshu <deepanshu.9dd@gmail.com>
2024-06-11 14:37:07 +0530

    chagne

--- src/pages/end/end.js
+++ src/pages/end/end.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@latest/dist/index.esm.js';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 // rest of your code
 
